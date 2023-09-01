8203976b2ae24df27a4567c9b4b6d4463b8c5aa0
Alan Miguel Sesin Abullarade <90735990+Cortis9@users.noreply.github.com>
2023-08-31 20:03:53 -0600

    Add files via upload

---
+++ src/pages/normal/Base.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import '../../styles/Base.css'
+
+export function Base() {
+
+  const navigate = useNavigate();
+  const auth = useAuth();
+  
+    const HandleLogout = () => {
+        auth.logout();
+        navigate("/");
+      }
+
+  return (
+
+ <header>
+  <a href="/home">
+ <img src="/src/assets/icono.png" height={50} width={50} id="logo"/>
+ </a>
+ <ul>
+ <li><a href="/Home">Home</a></li> 
+ <li><a href="/Evaluacion">Evaluación</a></li> 
+ <li><a href="/Resultados">Resultados</a></li>
+ </ul> 
+ 
+ <button onClick={()=> HandleLogout()} className="button" id="botonL">Logout</button>
+ </header>
+
+
+
+  );
+
+}
+
+
