056be091d7ce85a71c74245aaae796acd4f2be81
Alan Miguel Sesin Abullarade <90735990+Cortis9@users.noreply.github.com>
2023-09-02 11:45:54 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/library')) {
+            return 'my-library';
+          }
+        },
+      },
+    },
+  },
+
+
 })
