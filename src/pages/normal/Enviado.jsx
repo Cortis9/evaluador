5c6bb69c054228bbaa802d0964a7b0048910e062
Alan Miguel Sesin Abullarade <90735990+Cortis9@users.noreply.github.com>
2023-09-18 13:33:06 -0600

    Add files via upload

--- src/pages/normal/Enviado.jsx
+++ src/pages/normal/Enviado.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Base } from "../normal/Base";
 import '../../styles/normal/Enviado.css'
 import leonel from '../../assets/giphy.gif';
@@ -7,11 +6,6 @@ import leonel from '../../assets/giphy.gif';
 
 export const Enviado = () => {
   
-  const navigate = useNavigate();
-
-  const handleGoBack = () => {
-    navigate('/Home');
-  };
 
   return (
     <>
@@ -21,7 +15,7 @@ export const Enviado = () => {
     <h1 id='h1'>Evaluación Enviada</h1>
     <div> <img src={leonel} width={500} id='leonel'/></div>
    
-    <button onClick={handleGoBack} id='btr'>Home</button>
+
 
     </div>
     </>
